88dfc55c5fa9df744816c9d9c76b622db5837939
USER0308 <632991940@qq.com>
2019-03-04 16:17:53 +0800

    basic

---
+++ js/javascript.js
@@ -0,0 +1,72 @@
+
+
+function search(){
+
+  var selecter = document.getElementById("selectpicker")
+  // console.log(selecter.value)
+  var keyword = document.getElementById("keyword").value
+
+  keyword = keyword.Trim()
+  if(keyword.startWith("百度 ")){
+    selecter.value = "百度"
+  }else if(keyword.startWith("谷歌 ")){
+    selecter.value = "谷歌"
+  }else if(keyword.startWith("duckduckgo ")){
+    selecter.value = "duckdgo"
+  }else if(keyword.startWith("必应 ")){
+    selecter.value = "必应"
+  }else if(keyword.startWith("github ")){
+    selecter.value = "github"
+  }else if(keyword.startWith("知乎 ")){
+    selecter.value = "知乎"
+  }else if(keyword.startWith("csdn ")){
+    selecter.value = "csdn"
+  }else if(keyword.startWith("简书 ")){
+    selecter.value = "简书"
+  }else if(keyword.startWith("oschina ")){
+    selecter.value = "oschina"
+  }else if(keyword.startWith("bilibili")){
+    selecter.value = "bilibili"
+  }
+  
+  
+  var url = ""
+  switch(selecter.value){
+    case "百度": url = "https://baidu.com/s?wd="; break;
+    case "谷歌": url = "https://google.com/search?q="; break;
+    case "duckdgo": url = "https://duckduckgo.com/?q="; break;
+    case "必应": url = "https://cn.bing.com/search?q="; break;
+    case "github": url = "https://github.com/search?q="; break;
+    case "知乎": url = "https://www.zhihu.com/search?type=content&q="; break;
+    case "csdn": url = "http://so.csdn.net/so/search/s.do?q="; break;
+    case "简书": url = "https://www.jianshu.com/search?q="; break;
+    case "oschina": url = "https://www.oschina.net/search?scope=project&q="; break;
+    case "bilibili": url = "https://search.bilibili.com/all?keyword="; break;
+    default: url = "https://baidu.com/s?wd=";
+  }
+
+
+  // if(selecter.value === "百度"){
+  //   url = "https://baidu.com/s?wd="
+  // } else if(selecter.value === "谷歌"){
+  //   url = "https://google.com/search?q="
+  // }else if(selecter.value === "duckdgo"){
+  //   url = "https://duckduckgo.com/?q="
+  // }else if(selecter.value === "必应"){
+  //   url = "https://cn.bing.com/search?q="
+  // }else if(selecter.value === "github"){
+  //   url = "https://github.com/search?q="
+  // }else if(selecter.value === "知乎"){
+  //   url = "https://www.zhihu.com/search?type=content&q="
+  // }else if(selecter.value === "csdn"){
+  //   url = "http://so.csdn.net/so/search/s.do?q="
+  // }else if(selecter.value === "简书"){
+  //   url = "https://www.jianshu.com/search?q="
+  // }else if(selecter.value === "oschina"){
+  //   url = "https://www.oschina.net/search?scope=project&q="
+  // }
+
+  url = url + keyword
+  window.open(url, "_blank")
+  document.getElementById('keyword').value = ''
+}
