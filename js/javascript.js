f00796a3ba24e17eac91be866c04167eeaa12754
USER0308 <632991940@qq.com>
2019-03-04 18:37:19 +0800

    add 码云 博客园etc

--- js/javascript.js
+++ js/javascript.js
@@ -11,18 +11,28 @@ function search(){
     selecter.value = "百度"
   }else if(keyword.startWith("谷歌 ")){
     selecter.value = "谷歌"
+  }else if(keyword.startWith("stackof ")){
+    selecter.value = "stackOF"
+  }else if(keyword.startWith("wiki")){
+    selecter.value = "wiki"
   }else if(keyword.startWith("duckduckgo ")){
     selecter.value = "duckdgo"
   }else if(keyword.startWith("必应 ")){
     selecter.value = "必应"
   }else if(keyword.startWith("github ")){
     selecter.value = "github"
+  }else if(keyword.startWith("码云")){
+    selecter.value = "码云"
   }else if(keyword.startWith("知乎 ")){
     selecter.value = "知乎"
   }else if(keyword.startWith("csdn ")){
     selecter.value = "csdn"
   }else if(keyword.startWith("简书 ")){
     selecter.value = "简书"
+  }else if(keyword.startWith("掘金 ")){
+    selecter.value = "掘金"
+  }else if(keyword.startWith("博客园 ")){
+    selecter.value = "博客园"
   }else if(keyword.startWith("oschina ")){
     selecter.value = "oschina"
   }else if(keyword.startWith("bilibili")){
@@ -34,12 +44,17 @@ function search(){
   switch(selecter.value){
     case "百度": url = "https://baidu.com/s?wd="; break;
     case "谷歌": url = "https://google.com/search?q="; break;
+    case "stackOF": url = "http://stackoverflow.com/search?q="; break;
+    case "wiki": url = "https://en.wikipedia.org/wiki/"; break;
     case "duckdgo": url = "https://duckduckgo.com/?q="; break;
     case "必应": url = "https://cn.bing.com/search?q="; break;
     case "github": url = "https://github.com/search?q="; break;
+    case "码云": url = "https://gitee.com/search?q="; break;
     case "知乎": url = "https://www.zhihu.com/search?type=content&q="; break;
     case "csdn": url = "http://so.csdn.net/so/search/s.do?q="; break;
     case "简书": url = "https://www.jianshu.com/search?q="; break;
+    case "掘金": url = "https://juejin.im/search?query="; break;
+    case "博客园": url = "https://zzk.cnblogs.com/s?t=b&w="; break;
     case "oschina": url = "https://www.oschina.net/search?scope=project&q="; break;
     case "bilibili": url = "https://search.bilibili.com/all?keyword="; break;
     default: url = "https://baidu.com/s?wd=";
